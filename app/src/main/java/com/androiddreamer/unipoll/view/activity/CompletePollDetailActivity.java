476f3857c9f0a737b7deda8c744e6fe615c0fea4
Konstantinos Nikoloutsos <123123clapyourhands@gmail.com>
2020-05-25 20:36:58 +0300

    Finish pie UI and logic

--- app/src/main/java/com/androiddreamer/unipoll/view/activity/CompletePollDetailActivity.java
+++ app/src/main/java/com/androiddreamer/unipoll/view/activity/CompletePollDetailActivity.java
@@ -2,16 +2,34 @@ package com.androiddreamer.unipoll.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import com.androiddreamer.unipoll.Application;
 import com.androiddreamer.unipoll.R;
 import com.androiddreamer.unipoll.databinding.ActivityCompletePollDetailBinding;
+import com.androiddreamer.unipoll.util.UDHelper;
+import com.androiddreamer.unipoll.viewModel.PollDetailViewModel;
+import com.androiddreamer.unipoll.viewModel.PollsViewModel;
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -19,33 +37,73 @@ import java.util.List;
 
 public class CompletePollDetailActivity extends AppCompatActivity {
     ActivityCompletePollDetailBinding binding;
+    PollDetailViewModel viewModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_complete_poll_detail);
+        viewModel = ViewModelProviders.of(this).get(PollDetailViewModel.class);
 
-        List<BarEntry> entries = new ArrayList<>();
-        entries.add(new BarEntry(0f, 30f));
-        entries.add(new BarEntry(1f, 80f));
-        entries.add(new BarEntry(2f, 60f));
-        entries.add(new BarEntry(3f, 50f));
-        Collections.reverse(entries);
+        UDHelper udHelper = new UDHelper(getApplicationContext());
+        int pollId = getIntent().getExtras().getInt("id");
+        viewModel.getPollDetails(udHelper.getString(UDHelper.KEY_USER_ID), pollId)
+                .observe(this, new Observer<String>() {
+                    @Override
+                    public void onChanged(String s) {
+                        if(s == null) return;
+                        try{
+                            JSONObject webServerResponseJson = new JSONObject(s);
+                            JSONArray pollStats = webServerResponseJson.getJSONArray("poll_stats");
+                            renderBarPlot(pollStats);
+                        }catch (Exception e){
 
-        BarDataSet set = new BarDataSet(entries, "");
-        BarData data = new BarData(set);
-        data.setBarWidth(0.9f);
+                        }
+                    }
+                });
+    }
 
-        Description description = new Description();
-        description.setText("");
+    private void renderBarPlot(JSONArray json) throws JSONException {
+        PieChart pieChart = binding.pieChart;
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setExtraOffsets(5,10,5, 5);
+        pieChart.setDragDecelerationFrictionCoef(0.5f);
+        pieChart.setTouchEnabled(false);
 
-        binding.barChart.getAxisRight().setDrawLabels(false);
-        binding.barChart.getXAxis().setDrawLabels(false);
-        binding.barChart.setContentDescription("statistics");
-        binding.barChart.setDescription(description);
-        binding.barChart.setBackgroundColor(Color.WHITE);
-        binding.barChart.setData(data);
-        binding.barChart.setFitBars(true);
-        binding.barChart.invalidate();
+        pieChart.setUsePercentValues(true);
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setHoleColor(0x333333);
+        pieChart.setTransparentCircleRadius(60f);
+        pieChart.setTransparentCircleColor(0x333333);
+        pieChart.setDrawSliceText(false);
+
+        ArrayList<PieEntry> data = new ArrayList<>();
+        for (int i = 0; i < json.length(); i++) {
+            int totalVotesForThisOption = json.getJSONObject(i).getInt("votes");
+            int optionId = json.getJSONObject(i).getInt("option_id");
+            data.add(new PieEntry(totalVotesForThisOption, ""));
+        }
+
+
+        PieDataSet set = new PieDataSet(data, "");
+        set.setSliceSpace(8f);
+        set.setSelectionShift(12f);
+        set.setColors(ColorTemplate.JOYFUL_COLORS);
+        PieData pieData = new PieData(set);
+        pieChart.setData(pieData);
+        pieChart.getLegend().setEnabled(false);
+
+        for (int i = 0; i < json.length(); i++) {
+            String optionString = json.getJSONObject(i).getString("option");
+            View v = getLayoutInflater().inflate(R.layout.layout_option_statistics, null);
+
+            TextView optionTV = v.findViewById(R.id.option_tv);
+            View optionColor = v.findViewById(R.id.option_color);
+
+            optionTV.setText(optionString);
+            optionColor.setBackgroundColor(ColorTemplate.JOYFUL_COLORS[i % 5]);
+            binding.optionsLl.addView(v);
+        }
     }
 }
--- app/src/main/java/com/androiddreamer/unipoll/view/fragment/ActivePollListFragment.java
+++ app/src/main/java/com/androiddreamer/unipoll/view/fragment/ActivePollListFragment.java
@@ -52,38 +52,7 @@ public class ActivePollListFragment extends Fragment implements PollListAdapter.
     }
 
     private void loadPieData() {
-//        PieChart pieChart = binding.pieChart;
-//        pieChart.getDescription().setEnabled(false);
-//        pieChart.setExtraOffsets(5,10,5, 5);
-//        pieChart.setDragDecelerationFrictionCoef(0.5f);
-//
-//        pieChart.setEntryLabelColor(Color.WHITE);
-//
-//        pieChart.setTouchEnabled(false);
-//
-//
-//        pieChart.setUsePercentValues(true);
-//        pieChart.setDrawHoleEnabled(true);
-//        pieChart.setHoleColor(0x333333);
-//        pieChart.setTransparentCircleRadius(60f);
-//        pieChart.setTransparentCircleColor(0x333333);
-//
-//        ArrayList<PieEntry> data = new ArrayList<>();
-//        data.add(new PieEntry(22f, "A"));
-//        data.add(new PieEntry(44f, "B"));
-//        data.add(new PieEntry(11F, "C"));
-//
-//        pieChart.getLegend().setTextColor(Color.WHITE);
-//        pieChart.getLegend().setDirection(Legend.LegendDirection.RIGHT_TO_LEFT);
-//        pieChart.getLegend().setTextSize(40f);
-//
-//        PieDataSet set = new PieDataSet(data, "Poll_data_set_label");
-//        set.setSliceSpace(8f);
-//        set.setSelectionShift(12f);
-//        set.setColors(ColorTemplate.JOYFUL_COLORS);
-//        PieData pieData = new PieData(set);
-//        pieChart.setData(pieData);
-//        pieChart.spin(800, 0, -360f, Easing.EaseInOutQuad);
+
     }
 
     @Override
